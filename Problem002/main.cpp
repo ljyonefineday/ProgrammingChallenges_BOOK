bb716dcd3a6bf5e555e2022289d601464f5254eb
Jun-Young Lee <ljy.onefineday@snu.ac.kr>
2018-04-02 09:20:30 +0900

    from labdesktop

---
+++ Problem002/main.cpp
@@ -0,0 +1,8 @@
+//#include "stdafx.h"
+#include <iostream>
+int add(int x, int y); // needed so main.cpp knows that add() is a function declared elsewhere
+int main()
+{
+    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << std::endl;
+    return 0;
+}
