f90c0063865a6516ee9e56108094f3dbf9a63c34
JunYoung Lee <ljy.onefineday@snu.ac.kr>
2018-04-05 01:22:27 +0900

    problem 003 start

---
+++ Problem003/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void get_user_input(void);
+void average_calculate();
+void minimum_money_transfer();
+void printing_func();
+
+int main(void){
+
+    int how_many_people[]=0;
+         
+
+
+    get_user_input();
+    average_calculate();
+    minimum_money_transfer();
+    printing_func();
+
+    return 0;
+}
+
+void get_user_input(){
+    std::cin>>
+}
