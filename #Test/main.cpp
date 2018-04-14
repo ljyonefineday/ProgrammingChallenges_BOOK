b6d5fc807761da891d31433338a79f98c602d4fb
JunYoung Lee <ljy.onefineday@snu.ac.kr>
2018-04-14 23:01:02 +0900

    180414 test

--- #Test/main.cpp
+++ #Test/main.cpp
@@ -4,20 +4,19 @@ float* matrix_multi3331(float aa[3][3], float bb[3]){
     static float rslt[3]={0.0,0.0,0.0};
     for (int ii=0;ii<3;ii++){
         for(int jj=0;jj<3;jj++){
-            rslt[ii]=aa[ii][jj]*bb[jj];
+            rslt[ii]+=aa[ii][jj]*bb[jj];
         }
     }
     return rslt;
 }
 
-
-
 int main(void){
-    float aa[3][3]={{1,2,3},{4,5,6},{7,8.9}};
+    float aa[3][3]={{1,2,3},{4,5,6},{7,8,9}};
     float bb[3]={1,2,3};
     float *rslt;
 
     rslt=matrix_multi3331(aa,bb);
+    bb=*rslt;
 
     for( int ii=0;ii<3;ii++){
         
